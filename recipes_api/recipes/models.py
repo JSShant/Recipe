f715c8309211b9c752f1b4744a07a4a9dc2e5fb3
JSShant <joshua.shanthikumar@outlook.com>
2025-03-18 01:29:54 +1100

    Creating data models

--- recipes_api/recipes/models.py
+++ recipes_api/recipes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+class Ingredient(models.Model):
+    recipe = models.ForeignKey(Recipe, related_name="ingredients", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    quantity = models.FloatField()
+    unit = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.quantity} {self.unit} of {self.name}"
+
